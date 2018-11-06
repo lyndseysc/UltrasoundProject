820cf1e1d6effb123cfbefa540ef07cad0f8a2df
lyndseysc <35724145+lyndseysc@users.noreply.github.com>
2018-11-06 16:14:26 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -15,24 +15,21 @@ def reading():
     TRIG = 11
     ECHO = 12
 
-    # Disable any warning message such as GPIO pins in use
+    # Disable any warning message such as GPIO pins in use and set up the numbering system
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-     
+
+    #create a while loop so the code runs forever 
     while True:
  
-        # Setup the GPIO pins for TRIG and ECHO, including defining
-        # if these are input or output pins
+        # Setup the GPIO pins for TRIG and ECHO
          
         GPIO.setup(ECHO, GPIO.IN)
         GPIO.setup(TRIG, GPIO.OUT)
          
         time.sleep(0.3)
          
-        # sensor manual says a pulse length of 10Us will trigger the 
-        # sensor to transmit 8 cycles of ultrasonic burst at 40kHz and 
-        # wait for the reflected ultrasonic burst to be received
-         
+        
         # to get a pulse length of 10Us we need to start the pulse, then
         # wait for 10 microseconds, then stop the pulse. This will 
         # result in the pulse length being 10Us.
@@ -61,46 +58,30 @@ def reading():
          
         distance = (34000.0 * timepassed) * 0.5
         print(distance)
-        parameter = 10.0
-        GPIO.setmode(GPIO.BCM)
+        parameter = 10.0 #distance at which the LED turns on/camera starts recording
+
+        #Whilst the camera isn't working, the theory is demonstrated by flashing an LED instead of recording a video.    
         LED0 = 24
         if distance <= parameter:
             GPIO.setup(LED0, GPIO.OUT) # Set Pin as output
             GPIO.output(LED0, GPIO.HIGH) # Turn on the LED
             sleep(2)
-            GPIO.output(LED0, GPIO.LOW)
+            GPIO.output(LED0, GPIO.LOW) #Turn LED off
         sleep(5)
-        # return the distance of an object in front of the sensor in cm
-        #return distance
- 
-         
-        # we're no longer using the GPIO, so tell software we're done
-        #GPIO.cleanup()
+        
 
 '''
-def LED(distance):
-    while True:
-        parameter = 10.0
-        GPIO.setmode(GPIO.BCM)
-        LED0 = 24
         if distance <= parameter:
-            GPIO.setup(LED0, GPIO.OUT) # Set Pin as output
-            GPIO.output(LED0, GPIO.HIGH) # Turn on the LED
-            sleep(2)
-            GPIO.output(LED0, GPIO.LOW)
-
-        sleep(3)
+            camera.start_preview
+            sleep(10)
+            camera.stop_preview
 '''
-def camera():
-    parameter = 50.0
-    if distance <= parameter:
-        camera.start_preview()
-        sleep(10)
-        camera.stop_preview()
+        
+
+
+
         
 def main():
     distance = reading()
-    #camera(distance)
-    #LED(distance)
 main()
 
